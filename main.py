4d427245bbe01e6bf68df3483f4e2bd476172a8c
Fufigg <waitandbleed.s@gmail.com>
2021-11-20 18:03:42 +0300

    Отредактировали main.py чтобы проверить работу шифрования в строку и расшифровки этой же строки

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import yes_please
 
 s = input()
-result = yes_please.encrypt_string_to_string(s)
-print(result)
+encrypted = yes_please.encrypt_string_to_string(s)
+print(encrypted)
+
+decrypted = yes_please.decrypt_string_to_string(encrypted)
+print(decrypted)
